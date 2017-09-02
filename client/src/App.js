9fa5bb3614871f3d0b5f6e0f482e92c4b37b5530
Lukeghenco <lukeghenco@gmail.com>
2017-09-02 06:32:24 -0400

    Remove App.css and App.test.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Iron Starter</h1>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1>Iron Starter</h1>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
